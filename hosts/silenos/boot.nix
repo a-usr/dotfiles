29b2f31dd5ea66d7b3e8b37a39edd343af316edf
John Züchler <johnzuchler@gmail.com>
2024-08-07 21:09:43 +0200

    105 current  2024-08-07 21:09:40  24.05.20240805.883180e  6.6.44                          *

--- hosts/silenos/boot.nix
+++ hosts/silenos/boot.nix
@@ -2,6 +2,9 @@
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
     kernelModules = ["v4l2loopback"];
+    extraModprobeConfig = ''
+      options v4l2loopback card_label="LoopBack" exclusive_caps=1
+    '';
     loader = {
       efi = {
         canTouchEfiVariables = true;
