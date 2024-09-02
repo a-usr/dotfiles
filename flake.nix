256b43ed8ff1566cd927a9703ed0523941696c41
John <81042605+a-usr@users.noreply.github.com>
2024-09-02 18:31:53 +0200

    108 current  2024-08-30 19:11:32  24.05.20240805.883180e  6.6.44                          *

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
 
     ags.url = "github:Aylur/ags";
 
-    aquamarine.url = "github:hyprwm/aquamarine"; #"/07eb70afb131a4450aa01f5b488228c4cce6892b";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland/?submodules=1"; #&ref=explicit-sync&rev=158bbaaf88764e2a1e19ca1e3a11af541374432e";
+    #aquamarine.url = "github:hyprwm/aquamarine"; #"/07eb70afb131a4450aa01f5b488228c4cce6892b";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland/?submodules=1&ref=v0.42.0"; #&ref=explicit-sync&rev=158bbaaf88764e2a1e19ca1e3a11af541374432e";
     hyprland.inputs.nixpkgs.follows = "unstable";
-    hyprland.inputs.aquamarine.follows = "aquamarine";
+    #hyprland.inputs.aquamarine.follows = "aquamarine";
 
     hyprlock.url = "github:hyprwm/Hyprlock";
     hyprlock.inputs.nixpkgs.follows = "nixpkgs";
