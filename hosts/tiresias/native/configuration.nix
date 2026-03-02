3695d8831dda9e61a1a9014b3e7f956308904709
John <81042605+a-usr@users.noreply.github.com>
2026-03-02 19:40:26 +0100

    nat64 this time for sure

--- hosts/tiresias/native/configuration.nix
+++ hosts/tiresias/native/configuration.nix
@@ -1,12 +1,9 @@
 {
-  environment.etc."resolv.conf" = {
-    enable = true;
-    text = ''
-      	nameserver 2a00:1098:2b::1
-      	nameserver 2a01:4f8:c2c:123f::1
-      	nameserver 2a00:1098:2c::1
-      	'';
-  };
+  networking.nameservers = [
+    "2a00:1098:2b::1"
+    "2a01:4f8:c2c:123f::1"
+    "2a00:1098:2c::1"
+  ];
 
   nix.settings.trusted-users = [ "usr" ];
   system.stateVersion = "26.05";
